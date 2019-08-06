ab524863ac8be68c0b794259f22f1079616c2b58
LincolnStuart <lincoln.stuartlg@gmail.com>
2019-08-06 14:56:28 -0300

    Initial commit

---
+++ Service/Service/LoggerService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    class LoggerService : ServiceBase
+    {
+        private string filename;
+        private CancellationTokenSource _cts;
+
+        public LoggerService(string filename)
+        {
+            this.filename = filename;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            
+            _cts = new CancellationTokenSource();
+            WriteLog("service started");
+            Task.Factory.StartNew(() => InitService(), _cts.Token);
+        }
+
+        protected override void OnStop()
+        {
+            _cts.Cancel();
+            WriteLog("service stopped");
+        }
+
+        private void WriteLog(string e)
+        {
+            File.AppendAllText(filename, $"{e} - {DateTime.Now}{Environment.NewLine}");
+        }
+
+        private async void InitService()
+        {
+            while (true)
+            {
+                await Process();
+                _cts.Token.ThrowIfCancellationRequested();
+                Thread.Sleep(2000);
+            }
+        }
+
+        private async Task Process()
+        {
+            var tasks = Task.Factory.StartNew(() =>
+                {
+                    _cts.Token.ThrowIfCancellationRequested();
+                    WriteLog("  => processing");
+                });
+            _cts.Token.ThrowIfCancellationRequested();
+            await Task.WhenAll(tasks);
+        }
+    }
+}
---
+++ Service/Service/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+
+namespace Service
+{
+    class Program
+    {
+        private static readonly string filename = @"C:\Users\Public\Documents\service.txt";
+
+        static void Main(string[] args)
+        {
+            CreateFile();
+            using (var service = new LoggerService(filename))
+            {
+                ServiceBase.Run(service);
+            }
+        }
+
+        private static void CreateFile()
+        {
+            if (!File.Exists(filename))
+            {
+                File.Create(filename);
+            }
+        }
+    }
+}
